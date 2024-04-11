29cfc2a879fcc9d01cf7126cb5952115620f7490
Arnaud POINTET <arnaud.pointet@gmail.com>
2024-04-11 22:29:02 +0200

    Add speedInsights

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,5 +5,10 @@
       <slot />
     </UContainer>
     <footer class="h-[32px]"></footer>
+    <SpeedInsights />
   </div>
 </template>
+
+<script setup lang="ts">
+import { SpeedInsights } from '@vercel/speed-insights/next';
+</script>
