1d4bc94b8a15d6743dd8117f7c7ce4309db7f4cd
Arnaud POINTET <arnaud.pointet@gmail.com>
2024-04-12 10:42:00 +0200

    Fix List services

--- pages/services/index.vue
+++ pages/services/index.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
+import {useRouter} from "vue-router";
 import {useServices} from "~/composables/useServices";
 
+const router = useRouter()
 const services = useServices();
+
 </script>
 
 <template>
@@ -9,9 +12,9 @@ const services = useServices();
   <div class="grid grid-cols-2">
     <div
         v-for="service in services"
-        class="p-6 w-full flex flex-col hover:scale-105 hover:cursor-pointer transition ease-in-out" @click="router.push(url)">
+        class="p-6 w-full flex flex-col hover:scale-105 hover:cursor-pointer transition ease-in-out" @click="router.push(`/services/${service.slug}`)">
       <nuxt-img :src="service.image" alt="" loading="lazy" preload class="m-auto w-[200px] h-auto" />
-      <nuxt-link :to="url" class="text-bold text-center text-secondary">{{ service.name }}</nuxt-link>
+      <nuxt-link :to="`/services/${service.slug}`" class="text-bold text-center text-secondary">{{ service.name }}</nuxt-link>
     </div>
   </div>
 </template>
