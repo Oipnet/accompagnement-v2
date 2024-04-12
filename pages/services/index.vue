ae065eea3c8f7bc0a1a75b3d83833c0e7690a2bb
Arnaud POINTET <arnaud.pointet@gmail.com>
2024-04-12 10:37:39 +0200

    List services

--- pages/services/index.vue
+++ pages/services/index.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
+import {useServices} from "~/composables/useServices";
 
+const services = useServices();
 </script>
 
 <template>
-<h1>Services</h1>
+  <h1 class="flex justify-end text-4xl text-primary font-dancing-script w-full mt-12 pr-24">Services</h1>
+  <div class="grid grid-cols-2">
+    <div
+        v-for="service in services"
+        class="p-6 w-full flex flex-col hover:scale-105 hover:cursor-pointer transition ease-in-out" @click="router.push(url)">
+      <nuxt-img :src="service.image" alt="" loading="lazy" preload class="m-auto w-[200px] h-auto" />
+      <nuxt-link :to="url" class="text-bold text-center text-secondary">{{ service.name }}</nuxt-link>
+    </div>
+  </div>
 </template>
 
 <style scoped>
