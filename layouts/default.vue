a23061e4571914404d27defe5766e3440994ad5f
Arnaud POINTET <arnaud.pointet@gmail.com>
2024-04-12 11:04:01 +0200

    Fix Responsive

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,19 +5,27 @@
       <slot />
     </UContainer>
     <footer class="bg-gray-800 mt-8 text-white">
-      <div class="flex flex-col sm:flex-row">
-        <div class="m-8 w-full sm:w-1/3">
+      <div class="flex flex-col sm:flex-row pt-4">
+        <div class="w-full sm:w-1/3">
           <h2 class="text-center text-primary font-dancing-script mb-2 text-2xl">Suivez-moi</h2>
-          <UDivider></UDivider>
+          <UDivider :ui="{
+            wrapper: {
+              base: 'pl-8 pr-8'
+            }
+          }"></UDivider>
           <div class="flex flex-row gap-4 mt-4 mb-8 justify-around">
             <nuxt-link v-for="social in socials" :key="social.id" :to="social.url" target="_blank">
               <nuxt-img :src="social.icon" :alt="social.name" width="32px" height="32px" loading="lazy" preload></nuxt-img>
             </nuxt-link>
           </div>
         </div>
-        <div class="m-8 w-full sm:w-1/3">
+        <div class="w-full sm:w-1/3">
           <h2 class="text-center text-primary font-dancing-script mb-2 text-2xl">Vous et Moi</h2>
-          <UDivider></UDivider>
+          <UDivider :ui="{
+            wrapper: {
+              base: 'pl-8 pr-8'
+            }
+          }"></UDivider>
           <ul class="text-center italic underline">
             <li class="flex flex-col">
               <nuxt-link to="/qui-suis-je" class="text-secondary mb-4 mt-2">Qui suis-je ?</nuxt-link>
@@ -27,9 +35,13 @@
             </li>
           </ul>
         </div>
-        <div class="m-8 w-full sm:w-1/3">
+        <div class="w-full sm:w-1/3">
           <h2 class="text-center text-primary font-dancing-script mb-2 text-2xl">Pratique</h2>
-          <UDivider></UDivider>
+          <UDivider :ui="{
+            wrapper: {
+              base: 'pl-8 pr-8'
+            }
+          }"></UDivider>
           <ul class="text-center italic underline">
             <li class="flex flex-col">
               <nuxt-link to="/contact" class="text-secondary mb-4">Me contacter</nuxt-link>
